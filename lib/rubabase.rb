1d337180b265600f2240302213e2126c49d97fb3
David Shimenko <davidshimenko@proton.me>
2024-02-15 01:03:10 -0700

    Project initialization commit.

---
+++ lib/rubabase.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubabase/version"
+
+module Rubabase
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rubabase_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubabase do
+  it "has a version number" do
+    expect(Rubabase::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
