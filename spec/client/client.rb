533390ecdce6f74b6b9ae6efb2caf762a9dce7b3
Shaffan <shaffan1996@gmail.com>
2024-04-18 19:02:07 -0400

    failing test casesgit status! yay :D

--- spec/client/client.rb
+++ spec/client/client.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative '../lib/rubabase/client'
+require_relative '../../lib/rubabase/client'
 
 RSpec.describe Rubabase::Client do
   let(:client) { Rubabase::Client.instance }
